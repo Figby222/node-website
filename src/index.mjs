c1fa2003a89d3b65abfafb6dc6bb49e5ee0e94da
Figby222 <figby222@outlook.com>
2024-08-30 13:59:07 -0700

    Set head info & send response status of 200
    Remove http import

--- src/index.mjs
+++ src/index.mjs
@@ -1,10 +1,12 @@
 import express from "express";
 const app = express();
 import "dotenv/config";
-import http from "node:http";
 import fs from "node:fs";
 
-app.get("/", (req, res) => res.send("Hi"));
+app.get("/", (req, res) => {
+    res.set("Content-Type", "text/html");
+    res.status(200).send("Hi");
+});
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Application is listening on port ${PORT}`))
