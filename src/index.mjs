7625c470cb933f9d6566f51a13b333d325675b36
Figby222 <figby222@outlook.com>
2024-08-30 14:11:17 -0700

    Serve index HTML file for root URL path

--- src/index.mjs
+++ src/index.mjs
@@ -5,7 +5,14 @@ import fs from "node:fs";
 
 app.get("/", (req, res) => {
     res.set("Content-Type", "text/html");
-    res.status(200).send("Hi");
+    fs.readFile("./src/index.html", (err, data) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        res.status(200).send("Hi" + data);
+    })
 });
 
 const PORT = process.env.PORT;
