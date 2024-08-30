9707bcaf5ac5926111bf758912a7ec94134b6714
Figby222 <figby222@outlook.com>
2024-08-30 14:19:50 -0700

    Add route to about page
    Extract logic serving HTML content into the
    function serveHTMLContent()

--- src/index.mjs
+++ src/index.mjs
@@ -3,29 +3,35 @@ const app = express();
 import "dotenv/config";
 import fs from "node:fs";
 
-app.get("/", (req, res) => {
-    res.set("Content-Type", "text/html");
-    fs.readFile("./src/index.html", (err, data) => {
+function serveHTMLContent(filePath, res) {
+    res.writeHead(200, {"Content-Type": "text/html" });
+    res.write("hi");
+
+    fs.readFile(filePath, (err, data) => {
         if (err) {
             console.log(err);
             return;
         }
 
-        res.status(200).send("Hi" + data);
+        res.write(data);
+        return res.end();
     })
+}
+
+app.get("/", (req, res) => {
+    const filePath = "./src/index.html"
+    serveHTMLContent(filePath, res);
 });
 
 app.get("/contact-me", (req, res) => {
-    res.set("Content-Type", "text/html");
-    fs.readFile("./src/contact-me.html", (err, data) => {
-        if(err) {
-            console.log(err);
-            return;
-        }
-
-        res.status(200).send("Hi" + data);
-    })
+    const filePath = "./src/contact-me.html";
+    serveHTMLContent(filePath, res);
 });
+
+app.get("/about", (req, res) => {
+    const filePath = "./src/about.html";
+    serveHTMLContent(filePath, res);
+})
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Application is listening on port ${PORT}`))
