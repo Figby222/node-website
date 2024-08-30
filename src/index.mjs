ed632e6d8c3a9bed2335ff45404a100db0001995
Figby222 <figby222@outlook.com>
2024-08-30 13:53:10 -0700

    Init express server

--- src/index.mjs
+++ src/index.mjs
@@ -4,34 +4,7 @@ import "dotenv/config";
 import http from "node:http";
 import fs from "node:fs";
 
-http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write("hi");
-    
-    let desiredFilePath = "./index.html";
+app.get("/", (req, res) => res.send("Hi"));
 
-    switch(req.url) {
-        case "/":
-            desiredFilePath = "./index.html";
-            break;
-        case "/contact-me":
-            desiredFilePath = "./contact-me.html";
-            break;
-        case "/about":
-            desiredFilePath = "./about.html";
-            break;
-        default:
-            desiredFilePath = "./404.html";
-            break;
-    }
-
-    fs.readFile(desiredFilePath, (err, data) => {
-        if (err) {
-            console.log(err);
-            return;
-        }
-
-        res.write(data);
-        return res.end();
-    })
-}).listen(8080);
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Application is listening on port ${PORT}`))
